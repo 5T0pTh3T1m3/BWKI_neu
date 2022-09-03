61f8288aa4de43ddbbd5abadb6d45dc2b070b659
BigSmoke908 <74255728+BigSmoke908@users.noreply.github.com>
2022-09-04 00:09:22 +0200

    Repository auf vernünftige Größe verkleinert :)

--- add_to_git_ignore.py
+++ add_to_git_ignore.py
@@ -1,7 +1,7 @@
 import os
 
 
-gitignore = open('.idea/.gitignore', 'a')
+gitignore = open('.gitignore', 'a')
 for file in os.listdir('neuFiltern/GPU/'):
     gitignore.write('\nneuFiltern/GPU/' + file)
 gitignore.close()
